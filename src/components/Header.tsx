80f7a1c80d949ad47aea46f9d795af166b8e66ab
hostplacaresycocinas <hostplacaresycocinas@gmail.com>
2026-06-12 12:00:26 -0300

    header logo, hero images mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -83,7 +83,7 @@ const Header = () => {
                 </div>
               </>
             ) : (
-              <div className='h-14 md:h-16 w-52 sm:w-56 lg:w-64'>
+              <div className='h-10 md:h-12 w-52 sm:w-56 lg:w-64'>
                 <Image
                   priority
                   className='h-full w-full object-contain object-left'
